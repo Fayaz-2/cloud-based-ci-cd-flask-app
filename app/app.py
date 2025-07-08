2b2e9869759c7a6ff940ca162bc9c90e5a06f9be
root <root@ip-172-31-29-225.ec2.internal>
2025-07-08 16:57:03 +0000

    First commit

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD Pipeline!"
+    return "Hello from Flask CI/CD Pipeline Using Ansible and Docker Container!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
